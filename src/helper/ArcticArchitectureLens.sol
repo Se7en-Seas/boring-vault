6064f06f72d8f972876009071b986f7d2e09feb1
crispymangoes <crispymangoes@gmail.com>
2024-04-01 16:45:02 -0700

    Add missing natspec in lens contract

--- src/helper/ArcticArchitectureLens.sol
+++ src/helper/ArcticArchitectureLens.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
 import {BoringVault, ERC20} from "src/base/BoringVault.sol";
@@ -9,6 +8,13 @@ import {FixedPointMathLib} from "@solmate/utils/FixedPointMathLib.sol";
 contract ArcticArchitectureLens {
     using FixedPointMathLib for uint256;
 
+    /**
+     * @dev Calculates the total assets held in the BoringVault for a given vault and accountant.
+     * @param boringVault The BoringVault contract.
+     * @param accountant The AccountantWithRateProviders contract.
+     * @return asset The ERC20 asset, `assets` is given in terms of.
+     * @return assets The total assets held in the vault.
+     */
     function totalAssets(BoringVault boringVault, AccountantWithRateProviders accountant)
         external
         view
@@ -22,6 +28,14 @@ contract ArcticArchitectureLens {
         assets = totalSupply.mulDivDown(rate, 10 ** shareDecimals);
     }
 
+    /**
+     * @dev Calculates the number of shares that will be received for a given deposit amount in the BoringVault.
+     * @param depositAsset The ERC20 asset being deposited.
+     * @param depositAmount The amount of the asset being deposited.
+     * @param boringVault The BoringVault contract.
+     * @param accountant The AccountantWithRateProviders contract.
+     * @return shares The number of shares that will be received.
+     */
     function previewDeposit(
         ERC20 depositAsset,
         uint256 depositAmount,
@@ -33,10 +47,23 @@ contract ArcticArchitectureLens {
         shares = depositAmount.mulDivDown(10 ** shareDecimals, accountant.getRateInQuote(depositAsset));
     }
 
+    /**
+     * @dev Retrieves the balance of shares for a given account in the BoringVault.
+     * @param account The address of the account.
+     * @param boringVault The BoringVault contract.
+     * @return shares The balance of shares for the account.
+     */
     function balanceOf(address account, BoringVault boringVault) external view returns (uint256 shares) {
         shares = boringVault.balanceOf(account);
     }
 
+    /**
+     * @dev Calculates the balance of a user in terms of asset for a given account in the BoringVault.
+     * @param account The address of the account.
+     * @param boringVault The BoringVault contract.
+     * @param accountant The AccountantWithRateProviders contract.
+     * @return assets The balance of assets for the account.
+     */
     function balanceOfInAssets(address account, BoringVault boringVault, AccountantWithRateProviders accountant)
         external
         view
@@ -49,11 +76,24 @@ contract ArcticArchitectureLens {
         assets = shares.mulDivDown(rate, 10 ** shareDecimals);
     }
 
+    /**
+     * @dev Retrieves the current exchange rate from the AccountantWithRateProviders contract.
+     * @param accountant The AccountantWithRateProviders contract.
+     * @return rate The current exchange rate.
+     */
     function exchangeRate(AccountantWithRateProviders accountant) external view returns (uint256 rate) {
         rate = accountant.getRate();
     }
-    // Functions check if contract is paused, if deposit asset is good, and if users allowance is good, also user balance
 
+    /**
+     * @dev Checks if a user's deposit meets certain conditions.
+     * @param account The address of the user.
+     * @param depositAsset The ERC20 asset being deposited.
+     * @param depositAmount The amount of the asset being deposited.
+     * @param boringVault The BoringVault contract.
+     * @param teller The TellerWithMultiAssetSupport contract.
+     * @return A boolean indicating if the user's deposit meets the conditions.
+     */
     function checkUserDeposit(
         address account,
         ERC20 depositAsset,
@@ -68,6 +108,14 @@ contract ArcticArchitectureLens {
         return true;
     }
 
+    /**
+     * @dev Checks if a user's deposit (with permit) meets certain conditions.
+     * @param account The address of the user.
+     * @param depositAsset The ERC20 asset being deposited.
+     * @param depositAmount The amount of the asset being deposited.
+     * @param teller The TellerWithMultiAssetSupport contract.
+     * @return A boolean indicating if the user's deposit meets the conditions.
+     */
     function checkUserDepositWithPermit(
         address account,
         ERC20 depositAsset,
@@ -80,7 +128,20 @@ contract ArcticArchitectureLens {
         return true;
     }
 
+    /**
+     * @dev Retrieves the unlock time for a user's shares in the TellerWithMultiAssetSupport contract.
+     * @param account The address of the user.
+     * @param teller The TellerWithMultiAssetSupport contract.
+     * @return time The unlock time for the user's shares.
+     */
     function userUnlockTime(address account, TellerWithMultiAssetSupport teller) external view returns (uint256 time) {
         time = teller.shareUnlockTime(account);
+    }
+
+    /**
+     * @notice Checks if the TellerWithMultiAssetDepositSupport contract is paused.
+     */
+    function isTellerPaused(TellerWithMultiAssetSupport teller) external view returns (bool) {
+        return teller.isPaused();
     }
 }
