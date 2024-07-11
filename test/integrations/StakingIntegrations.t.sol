0c5b1464b4c7bbf0deb38d65e092716946e19b58
crispymangoes <crispymangoes@gmail.com>
2024-07-11 14:54:56 -0700

    Refactor mantle staking test

--- test/integrations/StakingIntegrations.t.sol
+++ test/integrations/StakingIntegrations.t.sol
@@ -398,78 +398,76 @@ contract StakingIntegrationsTest is Test, MerkleTreeHelper {
         assertApproxEqAbs(address(boringVault).balance, 100e18, 1, "BoringVault should have withdrawn and got ETH.");
     }
 
-    // function testMantleIntegration() external {
-    //     deal(getAddress(sourceChain, "WETH"), address(boringVault), 100e18);
+    function testMantleIntegration() external {
+        deal(getAddress(sourceChain, "WETH"), address(boringVault), 100e18);
 
-    //     // unwrap weth
-    //     // mint swETH
-    //     // unstaking swETH
-    //     ManageLeaf[] memory leafs = new ManageLeaf[](8);
-    //     leafs[0] = ManageLeaf(getAddress(sourceChain, "WETH"), false, "withdraw(uint256)", new address[](0));
-    //     leafs[1] = ManageLeaf(mantleLspStaking, true, "stake(uint256)", new address[](0));
-    //     leafs[2] = ManageLeaf(address(METH), false, "approve(address,uint256)", new address[](1));
-    //     leafs[2].argumentAddresses[0] = mantleLspStaking;
-    //     leafs[3] = ManageLeaf(mantleLspStaking, false, "unstakeRequest(uint128,uint128)", new address[](0));
-    //     leafs[4] = ManageLeaf(mantleLspStaking, false, "claimUnstakeRequest(uint256)", new address[](0));
+        // unwrap weth
+        // mint swETH
+        // unstaking swETH
+        ManageLeaf[] memory leafs = new ManageLeaf[](8);
+        _addNativeLeafs(leafs);
+        _addMantleStakingLeafs(leafs);
 
-    //     bytes32[][] memory manageTree = _generateMerkleTree(leafs);
+        bytes32[][] memory manageTree = _generateMerkleTree(leafs);
 
-    //     manager.setManageRoot(address(this), manageTree[manageTree.length - 1][0]);
+        manager.setManageRoot(address(this), manageTree[manageTree.length - 1][0]);
 
-    //     ManageLeaf[] memory manageLeafs = new ManageLeaf[](4);
-    //     manageLeafs[0] = leafs[0];
-    //     manageLeafs[1] = leafs[1];
-    //     manageLeafs[2] = leafs[2];
-    //     manageLeafs[3] = leafs[3];
-    //     bytes32[][] memory manageProofs = _getProofsUsingTree(manageLeafs, manageTree);
+        ManageLeaf[] memory manageLeafs = new ManageLeaf[](4);
+        manageLeafs[0] = leafs[1];
+        manageLeafs[1] = leafs[2];
+        manageLeafs[2] = leafs[3];
+        manageLeafs[3] = leafs[4];
+        bytes32[][] memory manageProofs = _getProofsUsingTree(manageLeafs, manageTree);
 
-    //     address[] memory targets = new address[](4);
-    //     targets[0] = getAddress(sourceChain, "WETH");
-    //     targets[1] = mantleLspStaking;
-    //     targets[2] = address(METH);
-    //     targets[3] = mantleLspStaking;
+        address[] memory targets = new address[](4);
+        targets[0] = getAddress(sourceChain, "WETH");
+        targets[1] = getAddress(sourceChain, "mantleLspStaking");
+        targets[2] = getAddress(sourceChain, "METH");
+        targets[3] = getAddress(sourceChain, "mantleLspStaking");
 
-    //     bytes[] memory targetData = new bytes[](4);
-    //     targetData[0] = abi.encodeWithSignature("withdraw(uint256)", 100e18);
-    //     targetData[1] = abi.encodeWithSignature("stake(uint256)", 0);
-    //     targetData[2] = abi.encodeWithSignature("approve(address,uint256)", mantleLspStaking, type(uint256).max);
-    //     uint256 expectedMeth = 96846201237918440407;
-    //     targetData[3] = abi.encodeWithSignature("unstakeRequest(uint128,uint128)", expectedMeth, 0);
-    //     uint256[] memory values = new uint256[](4);
-    //     values[1] = 100e18;
-    //     address[] memory decodersAndSanitizers = new address[](4);
-    //     decodersAndSanitizers[0] = rawDataDecoderAndSanitizer;
-    //     decodersAndSanitizers[1] = rawDataDecoderAndSanitizer;
-    //     decodersAndSanitizers[2] = rawDataDecoderAndSanitizer;
-    //     decodersAndSanitizers[3] = rawDataDecoderAndSanitizer;
+        bytes[] memory targetData = new bytes[](4);
+        targetData[0] = abi.encodeWithSignature("withdraw(uint256)", 100e18);
+        targetData[1] = abi.encodeWithSignature("stake(uint256)", 0);
+        targetData[2] = abi.encodeWithSignature(
+            "approve(address,uint256)", getAddress(sourceChain, "mantleLspStaking"), type(uint256).max
+        );
+        uint256 expectedMeth = 96846201237918440407;
+        targetData[3] = abi.encodeWithSignature("unstakeRequest(uint128,uint128)", expectedMeth, 0);
+        uint256[] memory values = new uint256[](4);
+        values[1] = 100e18;
+        address[] memory decodersAndSanitizers = new address[](4);
+        decodersAndSanitizers[0] = rawDataDecoderAndSanitizer;
+        decodersAndSanitizers[1] = rawDataDecoderAndSanitizer;
+        decodersAndSanitizers[2] = rawDataDecoderAndSanitizer;
+        decodersAndSanitizers[3] = rawDataDecoderAndSanitizer;
 
-    //     // Go back in time, so that request can be finalized immediately.
-    //     vm.roll(block.number - 10_200);
-    //     manager.manageVaultWithMerkleVerification(manageProofs, decodersAndSanitizers, targets, targetData, values);
+        // Go back in time, so that request can be finalized immediately.
+        vm.roll(block.number - 10_200);
+        manager.manageVaultWithMerkleVerification(manageProofs, decodersAndSanitizers, targets, targetData, values);
 
-    //     uint256 withdrawRequestId = 1500;
+        uint256 withdrawRequestId = 1500;
 
-    //     _finalizeMantleRequest(2_000e18);
+        _finalizeMantleRequest(2_000e18);
 
-    //     manageLeafs = new ManageLeaf[](1);
-    //     manageLeafs[0] = leafs[4];
-    //     manageProofs = _getProofsUsingTree(manageLeafs, manageTree);
+        manageLeafs = new ManageLeaf[](1);
+        manageLeafs[0] = leafs[5];
+        manageProofs = _getProofsUsingTree(manageLeafs, manageTree);
 
-    //     targets = new address[](1);
-    //     targets[0] = mantleLspStaking;
+        targets = new address[](1);
+        targets[0] = getAddress(sourceChain, "mantleLspStaking");
 
-    //     targetData = new bytes[](1);
-    //     targetData[0] = abi.encodeWithSignature("claimUnstakeRequest(uint256)", withdrawRequestId);
-    //     values = new uint256[](1);
+        targetData = new bytes[](1);
+        targetData[0] = abi.encodeWithSignature("claimUnstakeRequest(uint256)", withdrawRequestId);
+        values = new uint256[](1);
 
-    //     decodersAndSanitizers = new address[](1);
-    //     decodersAndSanitizers[0] = rawDataDecoderAndSanitizer;
-    //     manager.manageVaultWithMerkleVerification(manageProofs, decodersAndSanitizers, targets, targetData, values);
+        decodersAndSanitizers = new address[](1);
+        decodersAndSanitizers[0] = rawDataDecoderAndSanitizer;
+        manager.manageVaultWithMerkleVerification(manageProofs, decodersAndSanitizers, targets, targetData, values);
 
-    //     assertApproxEqRel(
-    //         address(boringVault).balance, 100e18, 0.0005e18, "BoringVault should have withdrawn and got ETH."
-    //     );
-    // }
+        assertApproxEqRel(
+            address(boringVault).balance, 100e18, 0.0005e18, "BoringVault should have withdrawn and got ETH."
+        );
+    }
 
     // ========================================= HELPER FUNCTIONS =========================================
 
