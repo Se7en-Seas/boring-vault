b46b5ba087dd6ee70b4cca5281ba76d09b8677ea
crispymangoes <crispymangoes@gmail.com>
2024-08-12 10:28:15 -0700

    Create deploy script

---
+++ script/ArchitectureDeployments/Mainnet/DeployPumpBtc.s.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.21;
+
+import {DeployArcticArchitecture, ERC20, Deployer} from "script/ArchitectureDeployments/DeployArcticArchitecture.sol";
+import {AddressToBytes32Lib} from "src/helper/AddressToBytes32Lib.sol";
+import {MainnetAddresses} from "test/resources/MainnetAddresses.sol";
+
+// Import Decoder and Sanitizer to deploy.
+import {EtherFiLiquidEthDecoderAndSanitizer} from
+    "src/base/DecodersAndSanitizers/EtherFiLiquidEthDecoderAndSanitizer.sol";
+
+/**
+ *  source .env && forge script script/ArchitectureDeployments/Mainnet/DeployCanaryBtc.s.sol:DeployCanaryBtcScript --with-gas-price 30000000000 --slow --broadcast --etherscan-api-key $ETHERSCAN_KEY --verify
+ * @dev Optionally can change `--with-gas-price` to something more reasonable
+ */
+contract DeployPumpBtcScript is DeployArcticArchitecture, MainnetAddresses {
+    using AddressToBytes32Lib for address;
+
+    uint256 public privateKey;
+
+    // Deployment parameters
+    string public boringVaultName = "pump liquid BTC";
+    string public boringVaultSymbol = "liquidpumpBTC";
+    uint8 public boringVaultDecimals = 8;
+    address public owner = dev0Address;
+
+    function setUp() external {
+        privateKey = vm.envUint("ETHERFI_LIQUID_DEPLOYER");
+        vm.createSelectFork("mainnet");
+    }
+
+    function run() external {
+        // Configure the deployment.
+        configureDeployment.deployContracts = false;
+        configureDeployment.setupRoles = false;
+        configureDeployment.setupDepositAssets = true;
+        configureDeployment.setupWithdrawAssets = true;
+        configureDeployment.finishSetup = false;
+        configureDeployment.setupTestUser = false;
+        configureDeployment.saveDeploymentDetails = true;
+        configureDeployment.deployerAddress = deployerAddress;
+        configureDeployment.balancerVault = balancerVault;
+        configureDeployment.WETH = address(WETH);
+
+        // Save deployer.
+        deployer = Deployer(configureDeployment.deployerAddress);
+
+        // Define names to determine where contracts are deployed.
+        names.rolesAuthority = PumpBtcRolesAuthorityName;
+        names.lens = ArcticArchitectureLensName;
+        names.boringVault = PumpBtcName;
+        names.manager = PumpBtcManagerName;
+        names.accountant = PumpBtcAccountantName;
+        names.teller = PumpBtcTellerName;
+        names.rawDataDecoderAndSanitizer = PumpBtcDecoderAndSanitizerName;
+        names.delayedWithdrawer = PumpBtcDelayedWithdrawer;
+
+        // Define Accountant Parameters.
+        accountantParameters.payoutAddress = liquidPayoutAddress;
+        accountantParameters.base = WBTC;
+        // Decimals are in terms of `base`.
+        accountantParameters.startingExchangeRate = 1e8;
+        //  4 decimals
+        accountantParameters.managementFee = 0.015e4;
+        accountantParameters.performanceFee = 0;
+        accountantParameters.allowedExchangeRateChangeLower = 0.995e4;
+        accountantParameters.allowedExchangeRateChangeUpper = 1.005e4;
+        // Minimum time(in seconds) to pass between updated without triggering a pause.
+        accountantParameters.minimumUpateDelayInSeconds = 1 days / 4;
+
+        // Define Decoder and Sanitizer deployment details.
+        bytes memory creationCode = type(EtherFiLiquidEthDecoderAndSanitizer).creationCode; // TODO update deocder
+        bytes memory constructorArgs =
+            abi.encode(deployer.getAddress(names.boringVault), uniswapV3NonFungiblePositionManager);
+
+        // Setup extra deposit assets.
+        depositAssets.push(
+            DepositAsset({
+                asset: LBTC, // TODO add in Pump BTC token
+                isPeggedToBase: true,
+                rateProvider: address(0),
+                genericRateProviderName: "",
+                target: address(0),
+                selector: bytes4(0),
+                params: [bytes32(0), 0, 0, 0, 0, 0, 0, 0]
+            })
+        );
+        // Setup withdraw assets.
+        withdrawAssets.push(
+            WithdrawAsset({
+                asset: LBTC,
+                withdrawDelay: 3 days,
+                completionWindow: 7 days,
+                withdrawFee: 0,
+                maxLoss: 0.01e4
+            })
+        );
+
+        withdrawAssets.push(
+            WithdrawAsset({
+                asset: WBTC,
+                withdrawDelay: 3 days,
+                completionWindow: 7 days,
+                withdrawFee: 0,
+                maxLoss: 0.01e4
+            })
+        );
+
+        bool allowPublicDeposits = true;
+        bool allowPublicWithdraws = true;
+        uint64 shareLockPeriod = 1 days;
+        address delayedWithdrawFeeAddress = liquidPayoutAddress;
+
+        vm.startBroadcast(privateKey);
+
+        _deploy(
+            "PumpBtcDeployment.json",
+            owner,
+            boringVaultName,
+            boringVaultSymbol,
+            boringVaultDecimals,
+            creationCode,
+            constructorArgs,
+            delayedWithdrawFeeAddress,
+            allowPublicDeposits,
+            allowPublicWithdraws,
+            shareLockPeriod,
+            dev1Address
+        );
+
+        vm.stopBroadcast();
+    }
+}
