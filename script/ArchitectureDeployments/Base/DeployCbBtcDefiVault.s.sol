16c008d2990dc1623194105a85c801eb565b9fbe
crispymangoes <crispymangoes@gmail.com>
2024-09-05 12:56:28 -0700

    Deploy vault

---
+++ script/ArchitectureDeployments/Base/DeployCbBtcDefiVault.s.sol
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {DeployArcticArchitecture, ERC20, Deployer} from "script/ArchitectureDeployments/DeployArcticArchitecture.sol";
+import {AddressToBytes32Lib} from "src/helper/AddressToBytes32Lib.sol";
+import {BaseAddresses} from "test/resources/BaseAddresses.sol";
+
+// Import Decoder and Sanitizer to deploy.
+import {cbBtcDefiVaultDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/cbBtcDefiVaultDecoderAndSanitizer.sol";
+
+/**
+ *  source .env && forge script script/ArchitectureDeployments/Base/DeployCbBtcDefiVault.s.sol:DeployCbBtcDefiVaultScript --with-gas-price 3000000 --broadcast --etherscan-api-key $BASESCAN_KEY --verify
+ * @dev Optionally can change `--with-gas-price` to something more reasonable
+ */
+contract DeployCbBtcDefiVaultScript is DeployArcticArchitecture, BaseAddresses {
+    using AddressToBytes32Lib for address;
+
+    uint256 public privateKey;
+
+    // Deployment parameters
+    string public boringVaultName = "cbBTC DeFi Vault";
+    string public boringVaultSymbol = "cbBTCv";
+    uint8 public boringVaultDecimals = 8;
+    address public owner = dev0Address;
+
+    function setUp() external {
+        privateKey = vm.envUint("ETHERFI_LIQUID_DEPLOYER");
+        vm.createSelectFork("base");
+    }
+
+    function run() external {
+        // Configure the deployment.
+        configureDeployment.deployContracts = true;
+        configureDeployment.setupRoles = true;
+        configureDeployment.setupDepositAssets = true;
+        configureDeployment.setupWithdrawAssets = true;
+        configureDeployment.finishSetup = true;
+        configureDeployment.setupTestUser = true;
+        configureDeployment.saveDeploymentDetails = true;
+        configureDeployment.deployerAddress = deployerAddress;
+        configureDeployment.balancerVault = balancerVault;
+        configureDeployment.WETH = address(WETH);
+
+        // Save deployer.
+        deployer = Deployer(configureDeployment.deployerAddress);
+
+        // Define names to determine where contracts are deployed.
+        names.rolesAuthority = CbBtcDefiVaultRolesAuthorityName;
+        names.lens = ArcticArchitectureLensName;
+        names.boringVault = CbBtcDefiVaultName;
+        names.manager = CbBtcDefiVaultManagerName;
+        names.accountant = CbBtcDefiVaultAccountantName;
+        names.teller = CbBtcDefiVaultTellerName;
+        names.rawDataDecoderAndSanitizer = CbBtcDefiVaultDecoderAndSanitizerName;
+        names.delayedWithdrawer = CbBtcDefiVaultDelayedWithdrawer;
+
+        // Define Accountant Parameters.
+        accountantParameters.payoutAddress = liquidPayoutAddress;
+        accountantParameters.base = cbBTC;
+        // Decimals are in terms of `base`.
+        accountantParameters.startingExchangeRate = 1e8;
+        //  4 decimals
+        accountantParameters.managementFee = 0.015e4;
+        accountantParameters.performanceFee = 0;
+        accountantParameters.allowedExchangeRateChangeLower = 0.995e4;
+        accountantParameters.allowedExchangeRateChangeUpper = 1.005e4;
+        // Minimum time(in seconds) to pass between updated without triggering a pause.
+        accountantParameters.minimumUpateDelayInSeconds = 1 days / 4;
+
+        // Define Decoder and Sanitizer deployment details.
+        bytes memory creationCode = type(cbBtcDefiVaultDecoderAndSanitizer).creationCode;
+        bytes memory constructorArgs =
+            abi.encode(deployer.getAddress(names.boringVault), uniswapV3NonFungiblePositionManager);
+
+        // Setup extra deposit assets.
+        depositAssets.push(
+            DepositAsset({
+                asset: tBTC,
+                isPeggedToBase: true,
+                rateProvider: address(0),
+                genericRateProviderName: "",
+                target: address(0),
+                selector: bytes4(0),
+                params: [bytes32(0), 0, 0, 0, 0, 0, 0, 0]
+            })
+        );
+        depositAssets.push(
+            DepositAsset({
+                asset: dlcBTC,
+                isPeggedToBase: true,
+                rateProvider: address(0),
+                genericRateProviderName: "",
+                target: address(0),
+                selector: bytes4(0),
+                params: [bytes32(0), 0, 0, 0, 0, 0, 0, 0]
+            })
+        );
+        // Setup withdraw assets.
+        withdrawAssets.push(
+            WithdrawAsset({
+                asset: cbBTC,
+                withdrawDelay: 3 days,
+                completionWindow: 7 days,
+                withdrawFee: 0,
+                maxLoss: 0.01e4
+            })
+        );
+        withdrawAssets.push(
+            WithdrawAsset({
+                asset: tBTC,
+                withdrawDelay: 3 days,
+                completionWindow: 7 days,
+                withdrawFee: 0,
+                maxLoss: 0.01e4
+            })
+        );
+
+        withdrawAssets.push(
+            WithdrawAsset({
+                asset: dlcBTC,
+                withdrawDelay: 3 days,
+                completionWindow: 7 days,
+                withdrawFee: 0,
+                maxLoss: 0.01e4
+            })
+        );
+
+        bool allowPublicDeposits = true;
+        bool allowPublicWithdraws = true;
+        uint64 shareLockPeriod = 1 days;
+        address delayedWithdrawFeeAddress = liquidPayoutAddress;
+
+        vm.startBroadcast(privateKey);
+
+        _deploy(
+            "cbBtcDefiVaultDeployment.json",
+            owner,
+            boringVaultName,
+            boringVaultSymbol,
+            boringVaultDecimals,
+            creationCode,
+            constructorArgs,
+            delayedWithdrawFeeAddress,
+            allowPublicDeposits,
+            allowPublicWithdraws,
+            shareLockPeriod,
+            dev1Address
+        );
+
+        vm.stopBroadcast();
+    }
+}
