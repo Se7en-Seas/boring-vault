59d7644fb4f389f86bc647414ffb21c074e0c937
crispymangoes <crispymangoes@gmail.com>
2024-09-17 12:11:42 -0700

    Create very basic merkle roots

---
+++ script/MerkleRootCreation/Base/CreateCbBTCMerkleRoot.s.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.21;
+
+import {FixedPointMathLib} from "@solmate/utils/FixedPointMathLib.sol";
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+import {Strings} from "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
+import {ERC4626} from "@solmate/tokens/ERC4626.sol";
+import {ManagerWithMerkleVerification} from "src/base/Roles/ManagerWithMerkleVerification.sol";
+import {MerkleTreeHelper} from "test/resources/MerkleTreeHelper/MerkleTreeHelper.sol";
+import "forge-std/Script.sol";
+
+/**
+ *  source .env && forge script script/MerkleRootCreation/Base/CreateCbBTCMerkleRoot.s.sol:CreateCbBTCMerkleRootScript --rpc-url $BASE_RPC_URL
+ */
+contract CreateCbBTCMerkleRootScript is Script, MerkleTreeHelper {
+    using FixedPointMathLib for uint256;
+
+    address public boringVault = 0x42A03534DBe07077d705311854E3B6933dD6Af85;
+    address public managerAddress = 0xcb4647c77688489655F45bB5bac42E14a0b05F85;
+    address public accountantAddress = 0x1c217f17d57d3CCD1CB3d8CB16B21e8f0b544156;
+    address public rawDataDecoderAndSanitizer = 0xA6b52921652A828Da445b457442F8cA10638a4Bb;
+
+    function setUp() external {}
+
+    /**
+     * @notice Uncomment which script you want to run.
+     */
+    function run() external {
+        /// NOTE Only have 1 function run at a time, otherwise the merkle root created will be wrong.
+        generateAdminStrategistMerkleRoot();
+    }
+
+    function generateAdminStrategistMerkleRoot() public {
+        setSourceChainName(base);
+        setAddress(false, base, "boringVault", boringVault);
+        setAddress(false, base, "managerAddress", managerAddress);
+        setAddress(false, base, "accountantAddress", accountantAddress);
+        setAddress(false, base, "rawDataDecoderAndSanitizer", rawDataDecoderAndSanitizer);
+
+        ManageLeaf[] memory leafs = new ManageLeaf[](32);
+
+        // ========================== UniswapV3 ==========================
+        address[] memory token0 = new address[](2);
+        token0[0] = getAddress(sourceChain, "cbBTC");
+        token0[1] = getAddress(sourceChain, "cbBTC");
+
+        address[] memory token1 = new address[](2);
+        token1[0] = getAddress(sourceChain, "dlcBTC");
+        token1[1] = getAddress(sourceChain, "tBTC");
+
+        _addUniswapV3Leafs(leafs, token0, token1);
+
+        // ========================== 1inch ==========================
+        address[] memory assets = new address[](3);
+        SwapKind[] memory kind = new SwapKind[](3);
+        assets[0] = getAddress(sourceChain, "cbBTC");
+        kind[0] = SwapKind.BuyAndSell;
+        assets[1] = getAddress(sourceChain, "dlcBTC");
+        kind[1] = SwapKind.BuyAndSell;
+        assets[2] = getAddress(sourceChain, "tBTC");
+        kind[2] = SwapKind.BuyAndSell;
+        _addLeafsFor1InchGeneralSwapping(leafs, assets, kind);
+
+        string memory filePath = "./leafs/Base/CbBTCStrategistLeafs.json";
+
+        bytes32[][] memory manageTree = _generateMerkleTree(leafs);
+
+        _generateLeafs(filePath, leafs, manageTree[manageTree.length - 1][0], manageTree);
+    }
+}
