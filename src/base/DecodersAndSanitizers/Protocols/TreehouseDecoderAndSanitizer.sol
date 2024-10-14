9e47320a6f7a6a926bdcdee5d20d2950748ae1b8
crispymangoes <crispymangoes@gmail.com>
2024-10-14 11:01:54 -0700

    Craete decoder and sanitizer

--- src/base/DecodersAndSanitizers/Protocols/TreehouseDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/Protocols/TreehouseDecoderAndSanitizer.sol
@@ -16,7 +16,13 @@ abstract contract TreehouseDecoderAndSanitizer is BaseDecoderAndSanitizer {
         addressesFound = abi.encodePacked(_asset);
     }
 
+    // Call on redemption contract https://etherscan.io/address/0x0618dbdb3be798346e6d9c08c3c84658f94ad09f#writeContract
     function redeem(uint96 /*_shares*/ ) external pure virtual returns (bytes memory addressesFound) {
+        return addressesFound;
+    }
+
+    // Call on redemption contract https://etherscan.io/address/0x0618dbdb3be798346e6d9c08c3c84658f94ad09f#writeContract
+    function finalizeRedeem(uint256 /*_redeemIndex*/ ) external pure virtual returns (bytes memory addressesFound) {
         return addressesFound;
     }
 }
