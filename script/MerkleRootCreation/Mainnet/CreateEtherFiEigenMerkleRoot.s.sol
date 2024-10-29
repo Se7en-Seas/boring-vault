353e102866e7f1f014aad691bb63e4f994110429
crispymangoes <crispymangoes@gmail.com>
2024-10-29 08:35:35 -0700

    Deploy updated decoder and create eEigen merkle root

--- script/MerkleRootCreation/Mainnet/CreateEtherFiEigenMerkleRoot.s.sol
+++ script/MerkleRootCreation/Mainnet/CreateEtherFiEigenMerkleRoot.s.sol
@@ -18,7 +18,7 @@ contract CreateEtherFiEigenMerkleRootScript is Script, MerkleTreeHelper {
     address public boringVault = 0xE77076518A813616315EaAba6cA8e595E845EeE9;
     address public managerAddress = 0x354ade0382EEC1BF0a444339ABc82931457C2c0e;
     address public accountantAddress = 0x075e60550C6f77f430B284E76aF699bC31651f75;
-    address public rawDataDecoderAndSanitizer = 0x0De55435028D904e1af8Ec58C2f86DF2c4d32f2a;
+    address public rawDataDecoderAndSanitizer = 0xb7Dd199ABE801cC4985B60B8B1365264Eb31ad26;
     address public itbDecoderAndSanitizer = 0xBF76C48401f7f690f46F0C481Ee9f193D0c43062;
 
     address public itbEigenPositionManager = 0xb814C334748dc8D12145b009020e2783624c0775;
