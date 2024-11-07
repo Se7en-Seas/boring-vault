731d10525c273018c8e522351d3358131711c359
crispymangoes <crispymangoes@gmail.com>
2024-11-07 14:57:08 -0800

    Fix refactor error

--- script/ArchitectureDeployments/Mainnet/DeploySommTestVault.s.sol
+++ script/ArchitectureDeployments/Mainnet/DeploySommTestVault.s.sol
@@ -60,7 +60,7 @@ contract DeploySommTestVaultScript is DeployArcticArchitecture, MainnetAddresses
         // Decimals are in terms of `base`.
         accountantParameters.startingExchangeRate = 1e18;
         //  4 decimals
-        accountantParameters.managementFee = 0;
+        accountantParameters.platformFee = 0;
         accountantParameters.performanceFee = 0;
         accountantParameters.allowedExchangeRateChangeLower = 0.995e4;
         accountantParameters.allowedExchangeRateChangeUpper = 1.005e4;
