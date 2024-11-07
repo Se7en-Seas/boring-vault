d9266ffea7801722d9b0b7c70cecf17ab5b95e61
mel0ndev <motherfker.jones1@gmail.com>
2024-11-07 11:35:33 -0500

    fix so decoding arrays

--- src/base/DecodersAndSanitizers/Protocols/AeraDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/Protocols/AeraDecoderAndSanitizer.sol
@@ -9,13 +9,15 @@ contract AeraDecoderAndSanitizer is BaseDecoderAndSanitizer {
     constructor(address _boringVault) BaseDecoderAndSanitizer(_boringVault) {}
 
     //add the registry to check for whitelisted assets? 
-    function deposit(DecoderCustomTypes.AssetValue calldata amounts) external pure virtual returns (bytes memory addressesFound) {
-    
-        //in this case, we would want to extract the token address and check it
-        return abi.encodePacked(amounts.asset); 
+    function deposit(DecoderCustomTypes.AssetValue[] calldata amounts) external pure virtual returns (bytes memory addressesFound) {
+        for (uint256 i = 0; i < amounts.length; i++) {
+            addressesFound = abi.encodePacked(amounts[i].asset); 
+        }
     }
 
-    function withdraw(DecoderCustomTypes.AssetValue calldata amounts) external pure virtual returns (bytes memory addressesFound) {
-        return abi.encodePacked(amounts.asset);  
+    function withdraw(DecoderCustomTypes.AssetValue[] calldata amounts) external pure virtual returns (bytes memory addressesFound) {
+        for (uint256 i = 0; i < amounts.length; i++) {
+            addressesFound = abi.encodePacked(amounts[i].asset); 
+        }
     }
 }
