0cc4bc900058a7dfba89e9bc7bfdf61998b7af67
mel0ndev <motherfker.jones1@gmail.com>
2024-11-11 18:21:44 -0500

    fix abstract constructor

--- src/base/DecodersAndSanitizers/Protocols/WeEthDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/Protocols/WeEthDecoderAndSanitizer.sol
@@ -5,8 +5,6 @@ import {BaseDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/BaseDecode
 
 abstract contract WeETHDecoderAndSanitizer is BaseDecoderAndSanitizer {
 
-    constructor(address _boringVault) BaseDecoderAndSanitizer(_boringVault) {}
-
     //add the registry to check for whitelisted assets? 
     function deposit(address tokenIn, uint256 /*amountIn*/, uint256 /*minAmountOut*/, address referral) external pure virtual returns (bytes memory addressesFound) {
         addressesFound = abi.encodePacked(tokenIn, referral); 
--- src/base/DecodersAndSanitizers/WeEthFullDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/WeEthFullDecoderAndSanitizer.sol
@@ -2,8 +2,9 @@
 pragma solidity 0.8.21;
 
 import {WeETHDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/Protocols/WeEthDecoderAndSanitizer.sol"; 
+import {BaseDecoderAndSanitizer} from "src/base/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol";
 
 contract WeETHFullDecoderAndSanitizer is WeETHDecoderAndSanitizer {
     
-    constructor(address _boringVault) WeETHDecoderAndSanitizer(_boringVault) {}
+    constructor(address _boringVault) BaseDecoderAndSanitizer(_boringVault) {}
 }
